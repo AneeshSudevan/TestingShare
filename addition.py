17af6cfafc96740738b890ad7bbff2de24a9231e
AneeshSudevan <35492063+AneeshSudevan@users.noreply.github.com>
2020-04-09 16:18:55 +0530

    Add files via upload

---
+++ addition.py
@@ -0,0 +1,33 @@
+
+#Normal Adding
+num1 = 15
+num2 = 12
+
+# Adding two nos
+sum = num1 + num2
+
+# printing values
+print("Sum of {0} and {1} is" .format(num1, num2,),num1+num2)
+
+#User Input
+
+input1=input("First Nummber ")
+input2=input("Second  Nummber ")
+sum =input1+input2
+print ("Sum of {0} and {1} is " .format(input1,input2),int(input1)+int(input2))
+
+#factorial of a number
+print("factorial of a number")
+
+inputnumber=input("Enter the number for which factoriar required ")
+nums=inputnumber
+total=1
+while nums<=1:
+    total=total*nums;
+    nums-=1
+print (" factorial of a {0}" .format(inputnumber),total)
+
+
+
+
+
